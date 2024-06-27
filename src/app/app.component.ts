5b4c217245b584c8417da26e3b78b88fb01e6187
yonastatek <yonastatekcse1@gmail.com>
2024-06-27 17:17:46 +0300

    added chat-bot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'glory-website-client';
-<<<<<<< HEAD
-
-=======
-   
->>>>>>> f5338362f21d00a245c612e4c137ce549ac274e1
 }
